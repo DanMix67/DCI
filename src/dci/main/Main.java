210bfb41b4ddb48caae584f7317fa9e56f039c16
Dan Mix <dmix@fandm.edu>
2017-07-15 16:41:04 -0400

    skeleton | Initial class creation

---
+++ src/dci/main/Main.java
@@ -0,0 +1,9 @@
+package dci.main;
+
+public class Main
+{
+    public static void Main(String[] args)
+    {
+
+    }
+}
---
+++ src/dci/shows/Performance.java
@@ -0,0 +1,15 @@
+package dci.shows;
+
+public class Performance
+{
+
+    /*
+     * Score earned at the show
+     */
+    private String score;
+
+    public Performance()
+    {
+
+    }
+}
